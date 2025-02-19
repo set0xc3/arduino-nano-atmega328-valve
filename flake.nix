aa81d706ff68d2517405f0a8189ea3c8e4213749
set0xc3 <set0xc3@gmail.com>
2025-02-19 19:34:21 +0300

    init

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+
+    venvDir = "venv";
+    pythonPackages = pkgs.pythonPackages;
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        bear
+        avrdude
+        platformio
+
+        pythonPackages.python
+      ];
+
+      shellHook = ''
+        export PYTHONPATH=$PWD/${venvDir}/${pythonPackages.python.sitePackages}/:$PYTHONPATH
+        export TMPDIR=$HOME/.cache/pip
+        export VENV_DIR=${venvDir}
+
+        if [ -d "${venvDir}" ]; then
+          echo "Skipping venv creation, '${venvDir}' already exists"
+        else
+          ${pythonPackages.python.interpreter} -m venv "${venvDir}"
+        fi
+
+        source "${venvDir}/bin/activate"
+      '';
+    };
+  };
+}
