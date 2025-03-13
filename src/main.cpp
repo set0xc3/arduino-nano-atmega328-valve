9c0e94d10d0b280852f82c7363ec484a6d68b9ed
set0xc3 <set0xc3@gmail.com>
2025-03-13 20:38:34 +0300

    backup

--- src/main.cpp
+++ src/main.cpp
@@ -1,134 +1,67 @@
 /*
-  
+
 */
 
-enum AppState {
-  Initializing,
+#include "Arduino.h"
+#include "timer.h"
+
+enum AppState
+{
   Running,
   Win,
   GameOver
 };
 
-#include "Arduino.h"
-#include "DFRobotDFPlayerMini.h"
+uint8_t digital_pin_led_a = PIND3;
+uint8_t digital_pin_win = PIND4;
+uint8_t digital_pin_game_over = PIND5;
 
-#include <SoftwareSerial.h>
-SoftwareSerial softSerial(/*rx =*/10, /*tx =*/11);
-#define FPSerial softSerial
-
-DFRobotDFPlayerMini myDFPlayer;
-void printDetail(uint8_t type, int value);
-
-int digitalPinWin = PIND4;
-int digitalPinGameOver = PIND5;
+AppState app_state = {};
+Timer timer_a = {};
 
 void setup()
 {
-  pinMode(digitalPinWin, INPUT_PULLUP);
-  pinMode(digitalPinGameOver, INPUT_PULLUP);
-
   Serial.begin(9600);
-
   Serial.println();
-  Serial.println(F("Initializing..."));
+  Serial.println(F("Setup..."));
 
-  // if (!myDFPlayer.begin(FPSerial, /*isACK = */ true, /*doReset = */ true))
-  // {
-  //   Serial.println(F("Unable to begin:"));
-  //   while (true)
-  //   {
-  //     delay(0); // Code to compatible with ESP8266 watch dog.
-  //   }
-  // }
-  // Serial.println(F("DFPlayer Mini online."));
+  timer_start(&timer_a);
 
-  // myDFPlayer.volume(10); // Set volume value. From 0 to 30
-  // myDFPlayer.play(1);    // Play the first mp3
+  pinMode(digital_pin_led_a, OUTPUT);
+  pinMode(digital_pin_win, INPUT_PULLUP);
+  pinMode(digital_pin_game_over, INPUT_PULLUP);
+
+  digitalWrite(digital_pin_led_a, HIGH);
+
+  app_state = Running;
+  Serial.println(F("Setup..."));
 }
 
 void loop()
 {
-  if (digitalRead(digitalPinWin) == LOW) {
-    Serial.println("НWin!");
-  } else  if (digitalRead(digitalPinGameOver) == LOW) {
-    Serial.println("GameOver!");
+  // Проверка состояний пинов
+  //
+  if (digitalRead(digital_pin_win) == LOW)
+  {
+    app_state = Win;
   }
 
-  // delay(100);
-
-  //  static unsigned long timer = millis();
-
-  //  if (millis() - timer > 5000) {
-  //    timer = millis();
-  //    myDFPlayer.next();  //Play next mp3 every 3 second.
-  //  }
-
-  // if (myDFPlayer.available())
-  // {
-  //   printDetail(myDFPlayer.readType(), myDFPlayer.read()); // Print the detail message from DFPlayer to handle different errors and states.
-  // }
-}
-
-void printDetail(uint8_t type, int value)
-{
-  switch (type)
+  if (digitalRead(digital_pin_game_over) == LOW)
   {
-  case TimeOut:
-    Serial.println(F("Time Out!"));
+    app_state = GameOver;
+  }
+
+  // Логика зависит от состояний пинов
+  //
+  switch (app_state)
+  {
+  case Running:
     break;
-  case WrongStack:
-    Serial.println(F("Stack Wrong!"));
+  case Win:
+    Serial.println("Win!");
     break;
-  case DFPlayerCardInserted:
-    Serial.println(F("Card Inserted!"));
-    break;
-  case DFPlayerCardRemoved:
-    Serial.println(F("Card Removed!"));
-    break;
-  case DFPlayerCardOnline:
-    Serial.println(F("Card Online!"));
-    break;
-  case DFPlayerUSBInserted:
-    Serial.println("USB Inserted!");
-    break;
-  case DFPlayerUSBRemoved:
-    Serial.println("USB Removed!");
-    break;
-  case DFPlayerPlayFinished:
-    Serial.print(F("Number:"));
-    Serial.print(value);
-    Serial.println(F(" Play Finished!"));
-    break;
-  case DFPlayerError:
-    Serial.print(F("DFPlayerError:"));
-    switch (value)
-    {
-    case Busy:
-      Serial.println(F("Card not found"));
-      break;
-    case Sleeping:
-      Serial.println(F("Sleeping"));
-      break;
-    case SerialWrongStack:
-      Serial.println(F("Get Wrong Stack"));
-      break;
-    case CheckSumNotMatch:
-      Serial.println(F("Check Sum Not Match"));
-      break;
-    case FileIndexOut:
-      Serial.println(F("File Index Out of Bound"));
-      break;
-    case FileMismatch:
-      Serial.println(F("Cannot Find File"));
-      break;
-    case Advertise:
-      Serial.println(F("In Advertise"));
-      break;
-    default:
-      break;
-    }
-    break;
-  default:
+  case GameOver:
+    Serial.println("GameOver!");
     break;
   }
 }
